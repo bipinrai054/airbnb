1b05d039b3ec6d8d2e9e7d1aa68861f540086c9f
bipnrai054 <bipinrai054@gmail.com>
2022-08-04 17:36:40 +0545

    destination navigation done

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -4,12 +4,16 @@ import styles from './styles';
 
 import Fontisto from 'react-native-vector-icons/Fontisto';
 
+import {useNavigation} from '@react-navigation/native';
+
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Pressable
         style={styles.searchButton}
-        onPress={() => console.log('search clicked')}>
+        onPress={() => navigation.navigate('Destination Search')}>
         <Fontisto name="search" size={25} color={'#f15454'} />
         <Text style={styles.searchButtonText}>Where are you going?</Text>
       </Pressable>
